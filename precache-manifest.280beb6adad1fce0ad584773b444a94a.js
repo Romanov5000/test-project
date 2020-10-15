fce08f4f60a8a6bec7b09ae6fbc33c3c0077b9dc
Romanov5000 <romanovvladislav97@gmail.com>
2020-10-15 17:46:38 +0300

    Updates

---
+++ precache-manifest.280beb6adad1fce0ad584773b444a94a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0e88d7897f9ec440c5c86bdea32eafa",
+    "url": "/test-project/index.html"
+  },
+  {
+    "revision": "dffa2d7212029668c834",
+    "url": "/test-project/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "e73508582fd06ea0909c",
+    "url": "/test-project/static/css/main.3bba78de.chunk.css"
+  },
+  {
+    "revision": "dffa2d7212029668c834",
+    "url": "/test-project/static/js/2.791f857b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test-project/static/js/2.791f857b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e73508582fd06ea0909c",
+    "url": "/test-project/static/js/main.927de8d7.chunk.js"
+  },
+  {
+    "revision": "879470d0160bf3f0562f",
+    "url": "/test-project/static/js/runtime-main.833cd9f1.js"
+  }
+]);
